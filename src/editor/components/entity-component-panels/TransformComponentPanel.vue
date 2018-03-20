f6d35e5684464d3455dd3205517121744dc6e97c
Ross Lote <ross.lote@boltlearning.com>
2018-03-20 13:09:44 +0000

    Removed obsolete code from transform editor.

--- src/editor/components/entity-component-panels/TransformComponentPanel.vue
+++ src/editor/components/entity-component-panels/TransformComponentPanel.vue
@@ -3,7 +3,7 @@
         <h5>Transform</h5>
         <table>
             <VectorEditor :vector="transform.localPosition" :label="'Position'" :setter="setPosition"/>
-            <VectorEditor :vector="transform.localScale" :label="'Scale'" :setter="setScale"/>
+            <VectorEditor :vector="transform.localScale" :label="'Scale'" :setter="setScale" :step="0.1"/>
             <ValueEditor :value="transform.localRotation" :label="'Angle'" :setter="setRotation" :step="0.01"/>
         </table>
     </div>
@@ -25,30 +25,8 @@ import { Vector2 } from '../../../engine/math/Vector2';
 })
 export default class TransformComponentPanel extends Vue {
 
-
-    // positionX: number = 0; 
-    // positionY: number = 0; 
-    // scaleX: number = 0; 
-    // scaleY: number = 0;
-    // rotation: number = 0;
-    // // transformWrapper: {transform: Transform | undefined} = {transform: undefined};
-    // // get transform(): Transform | undefined {
-    // //     return this.transformWrapper.transform;
-    // // }
-    // // set transform(transform: Transform | undefined) {
-    // //     this.transformWrapper.transform = transform;
-    // // }
-
     @Prop()
     transform: Transform;
-
-    // mounted() {
-    //     this.positionX = this.transform.getLocalPosition().x;
-    //     this.positionY = this.transform.getLocalPosition().y;
-    //     this.scaleX = this.transform.getLocalScale().x;
-    //     this.scaleY = this.transform.getLocalScale().y;
-    //     this.rotation = this.transform.getLocalRotation();
-    // }
 
     setPosition(vector: Vector2) {
         this.transform.setLocalPosition(vector);
@@ -61,24 +39,6 @@ export default class TransformComponentPanel extends Vue {
     setRotation(rotation: number) {
         this.transform.setLocalRotation(rotation);
     }
-
-    // @Watch('transform.localPosition')
-    // onTransformPositionChange(newVal: Vector2, oldVal: Vector2){
-    //     this.positionX = newVal.x;
-    //     this.positionY = newVal.y;
-    // }
-
-    // @Watch('scaleX')
-    // @Watch('scaleY')
-    // onScaleChange(newVal: number, oldVal: number) {
-    //     (<Transform>this.transform).setLocalScale([this.scaleX, this.scaleY]);
-    // }
-
-    // @Watch('rotation')
-    // onRotationChange(newVal: number, oldVal: number) {
-    //     (<Transform>this.transform).setLocalRotation(newVal);
-    // }
-
 }
 
 </script>
@@ -87,11 +47,7 @@ export default class TransformComponentPanel extends Vue {
 <style lang="less">
 
 .transform-component-panel{
-    table {
-        input {
-            width: 60px;
-        }
-    }
+
 }
 
 </style>
